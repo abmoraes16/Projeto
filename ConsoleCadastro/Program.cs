e2be08b711578d72ebcd7cdf2c07379dd4ce71de
FIC <a.bmoraes@outlook.com>
2017-10-05 21:02:51 -0300

    vscode-projeto-metodo/variaveis

---
+++ ConsoleCadastro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleCadastro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+
+            idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digitei: "+idade);
+        }
+    }
+}
